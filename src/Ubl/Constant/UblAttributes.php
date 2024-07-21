c56a265faa7b1a038bf8cc68cc0495122e415696
sean.kau <kliensheng2020@gmail.com>
2024-07-21 09:46:35 +0800

    - Add license and copyright in header for MIT license compliance

--- src/Ubl/Constant/UblAttributes.php
+++ src/Ubl/Constant/UblAttributes.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * @copyright Copyright (c) 2024 Sean Kau (kliensheng2020@gmail.com)
+ * @license https://github.com/klsheng/myinvois-php-sdk/blob/main/LICENSE
+ */
+
 namespace Klsheng\Myinvois\Ubl\Constant;
 
 class UblAttributes
